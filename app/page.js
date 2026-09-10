249a1435178196bcfee56a7f9deba4add60be309
geekstore100-hue <geekstore100@gmail.com>
2026-09-09 21:43:11 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '40px', textAlign: 'center' }}>
+      <h1>POS Geek Store</h1>
+      <p>Sistema en construcción.</p>
+    </main>
+  );
+}
